518e3a8945e2cf16cbb14d32d3d921435d90bdce
MartinKamauMaina888 <martin.maina01@student.moringaschool.com>
2022-11-27 14:39:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,95 @@
 // Write your solution here!
+
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+//.push()
+cats.push("Ralph");
+cats;
+
+//reset
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+//.unshift()
+cats.unshift("Bob");
+cats;
+
+//reset
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+//.pop()
+cats.pop()
+cats;
+
+//reset
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+ //.shift
+ cats.shift() 
+ cats;
+
+ //reset
+ beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  //.slice
+  cats.push("Broom");
+  cats;
+
+  cats.slice(0,3);
+  cats;
+
+  //reset
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+ 
+//.push
+cats.unshift("Arnold");
+cats;
+
+cats.slice(0);
+cats;
+
+//reset
+beforeEach(function () {
+  cats.length = 0;
+
+  cats.push("Milo", "Otis", "Garfield");
+});
+
+//.slice & ...Spread Operator
+const newCats = ["Milo", "Otis", ...cats.slice(2)];
+
+cats;
+newCats;
+
+//reset
+beforeEach(function () {
+  cats.length = 0;
+
+  cats.push("Milo", "Otis", "Garfield");
+});
+
+//.slice & ...Spread Operator
+const currentCats = [...cats.slice(0),"Otis", "Garfield"];
+
+cats;
+currentCats;
